a022474a78f540b44bc126f0be57718a0b85d1ec
Harikrishna-AL <amenu4aie20134@am.students.amrita.edu>
2024-01-23 18:55:37 +0530

    updated code with black

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ class Mistral7BChat:
         response = requests.post(self.api_endpoint, json=form_data, headers=self.headers)
 
         if response.status_code == 200:
-            output_text = response
+            output_text = response.json()
             # output_text = output_text.strip('"')
             # output_text = output_text.replace("\\n", "\n")
             # output_text = black.format_str(output_text, mode=black.FileMode())
